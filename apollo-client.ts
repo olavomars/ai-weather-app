888619c5e0f2df762c374ef05a1d37faf2c2df8c
Olavo Mars <olavoamneves@gmail.com>
2023-06-22 19:41:35 -0400

    adding fetchWeatherQueries function and implementing onto page component

--- apollo-client.ts
+++ apollo-client.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const getClient = () => {
   const client = new ApolloClient({
-    uri: 'process.env.API_URL',
+    uri: process.env.NEXT_PUBLIC_API_URL,
     cache: new InMemoryCache(),
     headers: {
       Authorization: `apikey ${process.env.NEXT_PUBLIC_STEPZEN_API_KEY}`,
--- app/location/[city]/[lat]/[long]/page.tsx
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -1,3 +1,6 @@
+import { getClient } from '@/apollo-client';
+import fetchWeatherQuery from '@/graphql/queries/fetchWeatherQueries';
+
 type Props = {
   params: {
     city: string;
@@ -6,7 +9,23 @@ type Props = {
   };
 };
 
-function WeatherPage({ params: { city, lat, long } }: Props) {
+async function WeatherPage({ params: { city, lat, long } }: Props) {
+  const client = getClient();
+
+  const { data } = await client.query({
+    query: fetchWeatherQuery,
+    variables: {
+      current_weather: 'true',
+      longitude: long,
+      latitude: lat,
+      timezone: 'GMT',
+    },
+  });
+
+  const results: Root = data.myQuery;
+
+  console.log(results);
+
   return (
     <div>
       Welcome to the weather Page: {city} {lat}
