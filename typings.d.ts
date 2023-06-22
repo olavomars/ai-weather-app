c4c1afac3bb30985a07c5acc8ca3bccbd6ba5f21
Olavo Mars <olavoamneves@gmail.com>
2023-06-21 23:06:26 -0400

    cleaning up typings

--- typings.d.ts
+++ typings.d.ts
@@ -1,4 +1,5 @@
 interface CurrentWeather {
+  is_day: number;
   temperature: number;
   time: string;
   weathercode: number;
@@ -7,16 +8,16 @@ interface CurrentWeather {
 }
 
 interface Daily {
-  apparent_temperature_max: [Float];
-  apparent_temperature_min: [Float];
-  sunrise: [DateTime];
-  sunset: [DateTime];
-  temperature_2m_max: [Float];
-  temperature_2m_min: [Float];
-  time: [Date];
-  uv_index_clear_sky_max: [Float];
-  uv_index_max: [Float];
-  weathercode: [Int];
+  apparent_temperature_max: [number];
+  apparent_temperature_min: [number];
+  sunrise: [string];
+  sunset: [string];
+  temperature_2m_max: [number];
+  temperature_2m_min: [number];
+  time: [string];
+  uv_index_clear_sky_max: [number];
+  uv_index_max: [number];
+  weathercode: [number];
 }
 
 interface DailyUnits {
@@ -33,19 +34,19 @@ interface DailyUnits {
 }
 
 interface Hourly {
-  apparent_temperature: [Float];
-  precipitation: [Float];
-  precipitation_probability: [Int];
-  rain: [Float];
-  relativehumidity_2m: [Int];
-  showers: [Float];
-  snow_depth: [Int];
-  snowfall: [Int];
-  temperature_2m: [Float];
-  time: [DateTime];
-  uv_index: [Float];
-  uv_index_clear_sky: [Float];
-  windgusts_10m: [Float];
+  apparent_temperature: [number];
+  precipitation: [number];
+  precipitation_probability: [number];
+  rain: [number];
+  relativehumidity_2m: [number];
+  showers: [number];
+  snow_depth: [number];
+  snowfall: [number];
+  temperature_2m: [number];
+  time: [string];
+  uv_index: [number];
+  uv_index_clear_sky: [number];
+  windgusts_10m: [number];
 }
 
 interface HourlyUnits {
@@ -68,13 +69,13 @@ interface Root {
   current_weather: CurrentWeather;
   daily: Daily;
   daily_units: DailyUnits;
-  elevation: Int;
-  generationtime_ms: Float;
+  elevation: number;
+  generationtime_ms: number;
   hourly: Hourly;
   hourly_units: HourlyUnits;
-  latitude: Float;
-  longitude: Float;
+  latitude: number;
+  longitude: number;
   timezone: String;
   timezone_abbreviation: String;
-  utc_offset_seconds: Int;
+  utc_offset_seconds: number;
 }
