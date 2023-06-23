abb31ae4ce5925646c53a4b3ffa63e9e906122da
Olavo Mars <olavoamneves@gmail.com>
2023-06-22 20:39:37 -0400

    adding callout card

--- app/location/[city]/[lat]/[long]/page.tsx
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -1,4 +1,5 @@
 import { getClient } from '@/apollo-client';
+import CalloutCard from '@/components/CalloutCard';
 import fetchWeatherQuery from '@/graphql/queries/fetchWeatherQueries';
 
 type Props = {
@@ -28,6 +29,23 @@ async function WeatherPage({ params: { city, lat, long } }: Props) {
 
   return (
     <div>
+      {/* {<informationPanel />} */}
+      <div>
+        <div className='pb-5'>
+          <div className='pb=5'>
+            <h2 className='text-xl font-bold'>Todays Overview</h2>
+            <p className='text-sm text-gray-400'>
+              Last Updated at:{''}
+              {new Date(results.current_weather.time).toLocaleString()} (
+              {results.timezone})
+            </p>
+          </div>
+        </div>
+
+        <div>
+          <CalloutCard message='this is where GPT summary will go!' />
+        </div>
+      </div>
       Welcome to the weather Page: {city} {lat}
     </div>
   );
